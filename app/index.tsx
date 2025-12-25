002ed995fd8db2e98c9ecf800d3797c6dbd88c5a
Hasanul-Alam <superdev0000@gmail.com>
2025-12-25 22:43:17 +0600

    update homepage text and add nativewind-env.d.ts to TypeScript configuration

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl text-center">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-2xl text-center bg-green-400 rounded-xl px-4 py-2">This is Homepage.</Text>
     </View>
   );
 }
